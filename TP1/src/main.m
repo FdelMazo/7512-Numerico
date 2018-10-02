2526c572b77df14c8d9c966198e36e0f2ae6e984
Federico del Mazo <federicodelmazo@hotmail.com>
2018-10-02 16:27:18 -0300

    Pair programming at fiuba

--- TP1/src/main.m
+++ TP1/src/main.m
@@ -1,31 +1,61 @@
 function integral = main
   padron1 = 100029; padron2 = 99779;
-  global P = (padron1 + padron2) / 50
+  global P = (padron1 + padron2) / 50;
+  global LIM_INF = 1; global LIM_SUP = 240; 
   global ALPHA = 0.17; global BETA = 0.41;
   global ERR_MAX = 10e-5;
-  global LIM_INF = 1; global LIM_SUP = 240; 
-  global MUS = 10e-8;
-  n = calcular_n(ERR_MAX)
-  global n = 10000000;
-  integral_d = 6.9458e+04;
-  integral_s = 7.0236e+04;
-  te = abs(calcular_te(integral_d, integral_s))
-  err = te.*MUS
-  tic
-  n = 10000
+  
+  n_sin_truncar = calcular_n(ERR_MAX)
+  n = 5000 
   integral = calcular_area(n)
-  toc
-  for i = 1:16;
-    perturbacion = 1/(10 .^i)
-    cpa = perturbarA(perturbacion,n)
-    cpb = perturbarB(perturbacion,n)
-   end
+  cps = calcular_cps(n);
+  cpa = cps(1), cpb = cps(2)
+  error_redondeo = calcular_err()
 end
 
-function te = calcular_te(d,s)
-  global MUS
-  te = (d.-s)./(d.*(MUS))
+function a = calcular_area(n)
+  global LIM_SUP LIM_INF;
+  h = ( LIM_SUP - LIM_INF ) ./ n;
+  f_inicio = f(LIM_INF) / 2;
+  f_fin = f(LIM_SUP) / 2;
+  f_i = 0;
+  for i = 1:n-1;
+    f_i = f_i + f(LIM_INF + i*h) * h;
+  end
+    a = ( f_inicio + f_fin ) * h + f_i;
 end
+
+function n = calcular_n(error_maximo_truncamiento)
+  global LIM_SUP LIM_INF
+  num = - ( (LIM_SUP - LIM_INF)^3 ) * fderivada2(1);
+  denom = error_maximo_truncamiento * 12;
+  n = sqrt(abs(num/denom));
+end
+
+##### TERMINO DE ESTABILIDAD Y CONDICION DEL PROBLEMA #####
+
+function err = calcular_err()
+  mu_single = 1.0000e-08;
+  integral_d = 6.9458e+04;
+  integral_s = 7.0236e+04;
+  te = (integral_d.-integral_s)./(integral_d.*(mu_single));
+  te = abs(te)
+  err = te.*mu_single;
+end
+
+function cps = calcular_cps(n)
+  cps_a = []; cps_b = [];
+  for i = 1:16;
+    perturbacion = 1/(10 .^i);
+    cps_a = [cps_a, perturbarA(perturbacion,n)];
+    cps_b = [cps_b, perturbarB(perturbacion,n)];
+   end
+   cpa = max(cps_a);
+   cpb = max(cps_b);
+   cps = [cpa,cpb];
+end
+
+##### PERTURBACIONES #####
 
 function cp = perturbarA(perturbacion,n)
   global ALPHA
@@ -46,6 +76,8 @@ function cp = perturbarB(perturbacion,n)
   BETA += perturbacion;
   cp =  abs((1 .- (valor_perturbado_inf ./ valor_perturbado_sup)) ./ perturbacion);
 end
+
+##### FUNCION Y SUS DERIVADAS #####
 
 function y = f(x)
   global P ALPHA BETA
@@ -69,42 +101,4 @@ function y = fderivada2(x)
   segundo_term = 2* sin(P.*x) ./ (ALPHA .* (x.^3) );
   tercer_term =  - ( ( (P^2)*sin(P.*x) ) ./ (ALPHA .* x) );
   y = abs(primer_term) + abs(segundo_term) + abs(tercer_term);
-end
-
-function n = calcular_n(error_maximo_truncamiento)
-  global LIM_SUP LIM_INF
-  num = - ( (LIM_SUP - LIM_INF)^3 ) * fderivada2(1);
-  denom = error_maximo_truncamiento * 12;
-  n = sqrt(abs(num/denom));
-end
-
-function a = calcular_area(n)
-  global LIM_SUP LIM_INF;
-  h = ( LIM_SUP - LIM_INF ) ./ n;
-  f_inicio = f(LIM_INF) / 2;
-  f_fin = f(LIM_SUP) / 2;
-  f_i = 0;
-  for i = 1:n-1;
-    f_i = f_i + f(LIM_INF + i*h) * h;
-  end
-    a = ( f_inicio + f_fin ) * h + f_i;
-end
-
-function g = graficar()
-  fplot(@f, [-0.02 0.02])
-  fplot(@fderivada, [-0.02 0.02])
-  fplot(@fderivada2, [-0.02 0.02])
-end
-
-function y = graficar_n()
-  x = [1,10,100,1000,10000,100000,1000000,10000000];
-  y = [];
-  for n = x
-    n
-    tic;
-    integral = calcular_area(n)
-    y = [y,toc]; 
-    printf("Tiempo = %ds\n\n",y(length(y)))   
-  end
-  plot(x,y,'o-r')
 end
--- TP1/src/mainsingle.m
+++ TP1/src/mainsingle.m
@@ -1,21 +1,46 @@
-function integral = singlemain
+function integral = mainsingle
   padron1 = single(100029); padron2 = single(99779);
   global P = (padron1 + padron2) / single(50)
+  global LIM_INF = single(1); global LIM_SUP = single(240); 
   global ALPHA = single(0.17); global BETA = single(0.41);
   global ERR_MAX = single(10e-5);
-  global LIM_INF = single(1); global LIM_SUP = single(240); 
-  #m = calcular_n(ERR_MAX)
-  tic
-  n = single(10000000)
+  
+  n_sin_truncar = calcular_n(ERR_MAX)
+  n = single(5000)
   integral = calcular_area(n)
-  toc
 end
+
+function a = calcular_area(n)
+  global LIM_SUP LIM_INF;
+  h = single(( LIM_SUP - LIM_INF ) ./ n);
+  f_inicio = single(f(LIM_INF) / 2);
+  f_fin = single(f(LIM_SUP) / 2);
+  f_i = 0;
+  for i = 1:n-1;
+    f_i = f_i + f(LIM_INF + i*h) * h;
+    f_i = single(f_i);
+  end
+    a = single(( f_inicio + f_fin ) * h + f_i);
+end
+
+function n = calcular_n(error_maximo_truncamiento)
+  global LIM_SUP LIM_INF
+  
+  num = - ( (LIM_SUP - LIM_INF)^3 ) * fderivada2(1);
+  num = single(num);
+  denom = error_maximo_truncamiento * 12;
+  denom = single(denom);
+  n = single(sqrt(abs(num/denom)));
+end
+
+##### FUNCION Y SUS DERIVADAS #####
 
 function y = f(x)
   x = single(x);
   global P ALPHA BETA
 
   y = ( single(sin(x.*P)) + BETA * (x.^2) ) ./ (x.*ALPHA);
+  y = single(y);
 end
 
 function y = fderivada(x)
@@ -26,6 +51,7 @@ function y = fderivada(x)
   segundo_term = - ( ( single(sin(P.*x)) ) ./ ( ALPHA .* (x.^2) ) );
   tercer_term = BETA / ALPHA;
   y = abs(single(primer_term)) + abs(single(segundo_term)) + abs(single(tercer_term));
+  y = single(y);
 end
 
 function y = fderivada2(x)
@@ -36,24 +62,5 @@ function y = fderivada2(x)
   segundo_term = 2* single(sin(P.*x)) ./ (ALPHA .* (x.^3) );
   tercer_term =  - ( ( (P^2)*single(sin(P.*x)) ) ./ (ALPHA .* x) );
   y = abs(single(primer_term)) + abs(single(segundo_term)) + abs(single(tercer_term));
-end
-
-function n = calcular_n(error_maximo_truncamiento)
-  global LIM_SUP LIM_INF
-  num = - ( (LIM_SUP - LIM_INF)^3 ) * fderivada2(1);
-  denom = error_maximo_truncamiento * 12;
-  n = single(sqrt(abs(num/denom)));
-end
-
-function a = calcular_area(n)
-  n = single(n);
-  global LIM_SUP LIM_INF;
-  h = ( LIM_SUP - LIM_INF ) ./ n;
-  f_inicio = f(LIM_INF) / 2;
-  f_fin = f(LIM_SUP) / 2;
-  f_i = 0;
-  for i = 1:n-1;
-    f_i = f_i + f(LIM_INF + i*h) * h;
-  end
-    a = ( f_inicio + f_fin ) * h + f_i;
+  y = single(y);
 end
